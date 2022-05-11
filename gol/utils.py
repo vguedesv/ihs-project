d1b738924e6aece30f49b68abda8f18181c95583
Vinicius Galindo Guedes <vgg@windows.cin.ufpe.br>
2022-05-11 10:57:36 -0300

    add utils

---
+++ gol/utils.py
@@ -0,0 +1,17 @@
+import os, sys
+from fcntl import ioctl
+from ioctl_cmds import *
+
+fd = os.open('/dev/mydev', os.O_RDWR) #substituir pelo caminho do arquivo
+
+def readButtons(){
+
+    ioctl(fd, RD_PBUTTONS)
+    red = os.read(fd, 4); # read 4 bytes and store in red var
+    print("red 0x%X"%int.from_bytes(red, 'little'))
+    print(red)
+}
+
+while(true){
+    readButtons()
+}
