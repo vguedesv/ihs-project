346c339a32896ca10c6d9ef5f16456cc97cf2922
Vinicius Galindo Guedes <vgg@WINDOWS.CIN.UFPE.BR>
2022-05-11 11:05:51 -0300

    fix {}

--- gol/utils.py
+++ gol/utils.py
@@ -4,14 +4,12 @@ from ioctl_cmds import *
 
 fd = os.open('/dev/mydev', os.O_RDWR) #substituir pelo caminho do arquivo
 
-def readButtons(){
-
+def readButtons():
     ioctl(fd, RD_PBUTTONS)
     red = os.read(fd, 4); # read 4 bytes and store in red var
     print("red 0x%X"%int.from_bytes(red, 'little'))
     print(red)
-}
 
-while(true){
+
+while(true):
     readButtons()
-}
