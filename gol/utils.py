bf93b3240fc32f57433354f00f0ced9eb856c917
Vinicius Galindo Guedes <vgg@WINDOWS.CIN.UFPE.BR>
2022-05-11 11:26:53 -0300

    switch case

--- gol/utils.py
+++ gol/utils.py
@@ -7,7 +7,19 @@ fd = os.open('/dev/mydev', os.O_RDWR) #substituir pelo caminho do arquivo
 def readButtons():
     ioctl(fd, RD_PBUTTONS)
     red = os.read(fd, 4); # read 4 bytes and store in red var
-    print(int.from_bytes(red, 'little'))
+    pressedButton  = int.from_bytes(red, 'little')
+    match pressedButton:
+    case 7:
+        return 'up'
+    case 11:
+        return 'down'
+    case 13:
+        return 'left'
+    case 14:
+        return 'right'
+    case _:
+        return ''
+
     print(red)
 
 
